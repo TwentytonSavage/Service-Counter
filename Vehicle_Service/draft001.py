b7beab847f996a3fac025a4c053c85b13c9f0a52
TwentytonSavage <kodahray@icloud.com>
2025-03-12 19:58:47 -0400

    Update draft001.py

--- Vehicle_Service/draft001.py
+++ Vehicle_Service/draft001.py
@@ -1,1 +1,5 @@
-input("Hello! What seems to be the issue with your vehicle today?: ")
+name = input("Hello, can I have your name?: ")
+info = input(f"Alright, {name}, can I have the make and model of you car please?: ")
+mileage = input(f"Whats the mileage on your, {info}?: ")
+issue = input(f"What seems to be the problem, {name}, with your, {info}?: ")
+
