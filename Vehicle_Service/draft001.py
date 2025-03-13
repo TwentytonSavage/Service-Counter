063b8bcaa9a658ad011f64f714d9bda63d899453
TwentytonSavage <kodahray@icloud.com>
2025-03-13 19:05:01 -0400

    Update draft001.py

--- Vehicle_Service/draft001.py
+++ Vehicle_Service/draft001.py
@@ -13,8 +13,12 @@ if issue.lower() == "oil change":
     print("That's 115.53 Canadian.")
     time.sleep(2)
     pay = input("Thank you for choosing Woonsocket Career Center auto garage, how would you like to pay?: ")
-
-
+else:
+    print("Sorry, we only offer oil changes, tire rotations, brake pad replacments, and broken glass repair.")
+    print("Thank you for choosing Woonsocket Career Center auto garage, have a great day!")
+    pay = None
+    
+if pay:
     if pay == ("Cash"):
         print(f"Thank you for your payment, {name}, have a great day!")
     elif pay == ("Credit"):
@@ -31,9 +35,8 @@ if issue.lower() == "oil change":
         print(f"Thank you for your payment, {name}, have a great day!")
     else:
         print("Sorry, we only accept cash, credit, or debit.")
-    
 
-elif issue.lower() == "tire rotation":
+if issue.lower() == "tire rotation":
     tire = input(f"Alright, {name}, what kind of tires do you have on your {info}?: ")
     if tire.lower() == "standard":
         print(f"Alright, {name}, that will be $30.00 for that type of tire rotation.")
@@ -46,6 +49,10 @@ elif issue.lower() == "tire rotation":
         print("That's 64.99 Canadian.")
         time.sleep(2)
         pay = input("Thank you for choosing Woonsocket Career Center auto garage, how would you like to pay?:")
+else:
+    print("Sorry, we only offer oil changes, tire rotations, brake pad replacments, and broken glass repair.")
+    print("Thank you for choosing Woonsocket Career Center auto garage, have a great day!")
+    
     if pay == ("Cash"):
         print(f"Thank you for your payment, {name}, have a great day!")
     elif pay == ("Credit"):
@@ -64,8 +71,7 @@ elif issue.lower() == "tire rotation":
         print("Sorry, we only accept cash, credit, or debit.")
     
 
-elif issue.lower() == "brake pad replacment":
-    
+if issue.lower() == "brake pad replacment":
     pads = input(f"Alright, {name}, would you like the standard which only does the brake pads or the package that does both brake pad replacemnet and front end alignment?: ")
     
     if pads.lower() == "standard":
@@ -79,6 +85,10 @@ elif issue.lower() == "brake pad replacment":
         time.sleep(2)
         print("That's 173.33 Canadian.")
         time.sleep(2)
+    else:
+        print("Sorry, we only offer oil changes, tire rotations, brake pad replacments, and broken glass repair.")
+        print("Thank you for choosing Woonsocket Career Center auto garage, have a great day!")
+    
     
     pay = input("Thank you for choosing Woonsocket Career Center auto garage, how would you like to pay?:")
     
@@ -91,13 +101,48 @@ elif issue.lower() == "brake pad replacment":
         print("Prossesing...")
         time.sleep(2)
         print(f"Thank you for your payment, {name}, have a great day!")
-   
+
     elif pay == ("Debit"):
         print("Alright just swipe your card here.")
         time.sleep(2)
         print("Prossesing...")
         time.sleep(2)
         print(f"Thank you for your payment, {name}, have a great day!")
-   
+
     else:
         print("Sorry, we only accept cash, credit, or debit.")
+
+elif issue.lower() == "broken glass repair":
+    glass = input(f"Alright, {name}, is it a large or a small window that needs to be repaired on your {info}?: ")
+    if glass.lower() == "front":
+        print(f"Alright, {name}, that will be $69.99 for the large window.")
+        time.sleep(2)
+        print("That's 101.08 Canadian.")
+        time.sleep(2)
+    elif glass.lower() == "back":
+        print(f"Alright, {name}, that will be $39.99 for the small window.")
+        time.sleep(2)
+        print("That's 57.76 Canadian.")
+        time.sleep(2)
+    pay = input("Thank you for choosing Woonsocket Career Center auto garage, how would you like to pay?:")
+    if pay == ("Cash"):
+        print(f"Thank you for your payment, {name}, have a great day!")
+    elif pay == ("Credit"):
+        print("Alright just swipe your card here.")
+        time.sleep(2)
+        print("Prossesing...")
+        time.sleep(2)
+        print(f"Thank you for your payment, {name}, have a great day!")
+    elif pay == ("Debit"):
+        print("Alright just swipe your card here.")
+        time.sleep(2)
+        print("Prossesing...")
+        time.sleep(2)
+        print(f"Thank you for your payment, {name}, have a great day!")
+    else:
+        print("Sorry, we only accept cash, credit, or debit.")
+else:
+    print("Sorry, we only offer oil changes, tire rotations, brake pad replacments, and broken glass repair.")
+    print("Thank you for choosing Woonsocket Career Center auto garage, have a great day!")
+    
+    
