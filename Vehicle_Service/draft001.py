4ab5d0089f2347898fb3f54569e33436664e9892
TwentytonSavage <kodahray@icloud.com>
2025-03-28 11:51:47 -0400

    Update draft001.py
    fixed minor spelling mistake

--- Vehicle_Service/draft001.py
+++ Vehicle_Service/draft001.py
@@ -1,7 +1,7 @@
 import time
 
 name = input("Hello, can I have your name?: ")
-info = input(f"Alright, {name}, can I have the make and model of you car please?: ")
+info = input(f"Alright, {name}, can I have the make and model of your car please?: ")
 mileage = input(f"Whats the mileage on your, {info}?: ")
 issue = input(f"What do you need done, {name}, with your {info}?: ")
 
